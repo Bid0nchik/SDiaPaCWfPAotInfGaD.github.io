aa3d9b85bb777e5c1675a7f0c2eab9b5772bc6c9
Bid0nchik <alexbidon03@mail.ru>
2026-01-18 19:34:21 +0500

    Update delete.js

--- backend/delete.js
+++ backend/delete.js
@@ -11,7 +11,7 @@ const serviceAccount = {
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
     token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs", 
     client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
 };
 try {
@@ -47,3 +47,4 @@ router.delete('/articles/:section/:id', async (req, res) => {
 });
 
 module.exports = router;
+
