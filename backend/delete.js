dfa89f80b6d9e200337db0fdf0260d318259a77b
Bid0nchik <alexbidon03@mail.ru>
2026-01-18 18:51:49 +0500

    Update delete.js

--- backend/delete.js
+++ backend/delete.js
@@ -1,5 +1,6 @@
 const express = require('express'); 
 const router = express.Router(); 
+const admin = require('firebase-admin');
 const db = admin.firestore();
 
 router.delete('/articles/:section/:id', async (req, res) => {
