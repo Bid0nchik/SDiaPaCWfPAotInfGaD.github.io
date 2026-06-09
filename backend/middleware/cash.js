a721d83f18d9ac95b9df10903f3c5c887ed3d087
Илья <alexbidon03@mail.ru>
2026-06-10 00:45:30 +0500

    Update

--- backend/middleware/cash.js
+++
@@ -1,8 +0,0 @@
-const app = require('express')();
-app.use((req, res, next) => {
-    if (req.method === 'GET') {
-        res.set('Cache-Control', 'public, max-age=30');
-    }
-    next();
-});
-module.exports = app;
--- backend/middleware/validSection.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(req,res,next){
-    const section = req.params.section;
-    if (!['Prog', 'OSINT', 'Trol'].includes(section)) {
-        return res.status(400).json({ 
-            error: 'Неверный раздел. Допустимые значения: Prog, OSINT, Trol' 
-        });
-    }
-    next();
-};
